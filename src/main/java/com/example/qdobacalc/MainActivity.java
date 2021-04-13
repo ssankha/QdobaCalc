1795b92e2439ed4ef249e6c5c6330716fb4350f2
ssankha <ssankha@purdue.edu>
2021-04-13 03:15:44 -0400

    Changed color of action bar

--- src/main/java/com/example/qdobacalc/MainActivity.java
+++ src/main/java/com/example/qdobacalc/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.qdobacalc;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,14 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#601400")));
     }
 
     public void onClick(View view) {
         switch(view.getId()) {
 
             case R.id.burrito_button:
-                Intent intent1 = new Intent(this, BurritoCalc.class);
-                startActivity(intent1);
+                Intent intent= new Intent(this, BurritoCalc.class);
+                startActivity(intent);
                 finish();
                 break;
         }
